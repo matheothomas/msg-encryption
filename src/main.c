88ef7da9eba36990601f815da849db946540f0bb
matheothomas <103116889+matheothomas@users.noreply.github.com>
2023-12-12 09:46:49 +0100

    Added socket support

--- src/main.c
+++ src/main.c
@@ -8,39 +8,72 @@
 #include <stdint.h>
 #include <stdbool.h>
 #include <string.h>
+
 #include <math.h>
 #include <complex.h>
 #include <time.h>
+
+#include <unistd.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+
 #include "rsa.h"
+#include "network.h"
 
 
-int main(void){
-	pubKey *pub = (pubKey *)malloc(sizeof(pubKey));
-	priKey *pri = (priKey *)malloc(sizeof(priKey));
-	genKeys(pub, pri);
-	printf("%lld\n%lld\n%lld\n%lld\n", pub->mod, pub->exp, pri->mod, pri->exp);
 
-	
-	char msg[] = "Coucou ceci est un test de l'algorithme de RSA !";
-	for(int i = 0; i < strlen(msg); i++){
-		printf("%c", (long long)msg[i]);
+int main(int argc, char *argv[]){
+
+	if(argc >= 2){
+		if(strcmp(argv[1], "server") == 0){
+			netParams p = mainNetworkServ();
+		} else if(strcmp(argv[1], "client") == 0){
+			if(argc >= 3){
+				char *IPADDR = argv[2];
+				netParams p = mainNetworkClient(IPADDR);
+			} else {
+				fprintf(stderr, "Not enough parameters.\n");
+			}
+		} else {
+			fprintf(stderr, "Wrong parameter.\n");
+		}
+	} else {
+		fprintf(stderr, "Not enough parameters.\n");
 	}
-	printf("\n");
-	long long *encrypted = encrypt(msg, sizeof(msg), pub);
-
-	for(int i = 0; i < strlen(msg); i++){
-		printf("%lld\n", (long long)encrypted[i]);
-	}
-
-	char *decrypted = decrypt(encrypted, 8*sizeof(msg), pri);
-	for(int i = 0; i < strlen(msg); i++){
-		printf("%c", (long long)decrypted[i]);
-	}
-	printf("\n");
 
 
-	free(pub);
-	free(pri);
+
+
+	/*
+	   pubKey *pub = (pubKey *)malloc(sizeof(pubKey));
+	   priKey *pri = (priKey *)malloc(sizeof(priKey));
+	   genKeys(pub, pri);
+	   printf("%lld\n%lld\n%lld\n%lld\n", pub->mod, pub->exp, pri->mod, pri->exp);
+
+
+	   char msg[] = "Coucou ceci est un test de l'algorithme de RSA !";
+	   for(int i = 0; i < strlen(msg); i++){
+	   printf("%c", (long long)msg[i]);
+	   }
+	   printf("\n");
+	   long long *encrypted = rsaEncrypt(msg, sizeof(msg), pub);
+
+	   for(int i = 0; i < strlen(msg); i++){
+	   printf("%lld\n", (long long)encrypted[i]);
+	   }
+
+	   char *decrypted = rsaDecrypt(encrypted, 8*sizeof(msg), pri);
+	   for(int i = 0; i < strlen(msg); i++){
+	   printf("%c", (long long)decrypted[i]);
+	   }
+	   printf("\n");
+
+
+	   free(pub);
+	   free(pri);
+	   */
+
 	return 0;
 }
 
